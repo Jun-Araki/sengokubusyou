94e2231570a69c5bd5b088ad7597081ff939787a
Jun-Araki <jun7772006@gmail.com>
2022-09-25 10:48:40 +0900

    新規投稿機能を実装+モデルpostのカラム名birhplaceを変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,11 +7,20 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-  def create; end
+  def create
+    post = Post.create!(post_params)
+    redirect_to post_path(post)
+  end
 
   def edit; end
 
   def update; end
 
   def destroy; end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:name, :catch_copy, :birthplace, :castle, :famous_battle, :recommend_point)
+  end
 end
