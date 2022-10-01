e4b71e7c18a4f19d7cff5e4889dc45bca537ee37
Jun-Araki <jun7772006@gmail.com>
2022-10-02 07:01:03 +0900

    いいねを非同期処理

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,11 @@
 class LikesController < ApplicationController
   def create
-    current_user.likes.create!(post_id: params[:post_id])
-    redirect_back(fallback_location: root_path)
+    @post = Post.find(params[:post_id])
+    current_user.likes.create!(post_id: @post.id)
   end
 
   def destroy
-    current_user.likes.find_by(post_id: params[:post_id]).destroy!
-    redirect_back(fallback_location: root_path)
+    @post = Post.find(params[:post_id])
+    current_user.likes.find_by(post_id: @post.id).destroy!
   end
 end
