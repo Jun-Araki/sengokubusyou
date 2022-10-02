c7c3aceba00623f371a162e7da0cf9f0974c2475
Jun-Araki <jun7772006@gmail.com>
2022-10-02 16:34:20 +0900

    ゲストログインのパスワード再設定無効化

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@
 Rails.application.routes.draw do
   root to: "posts#index"
   devise_for :users, controllers: {
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    passwords: "users/passwords"
   }
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
