7217495886637766eeb6d1a81f20a5a5f13ffa4b
Jun-Araki <jun7772006@gmail.com>
2022-10-02 15:12:39 +0900

    ゲストログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   root to: "posts#index"
   devise_for :users
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
   resources :posts do
     resource :likes, only: %i[create destroy]
   end
