8675a0e6cfc25711fa46418d0d208fdc6b855206
Jun-Araki <jun7772006@gmail.com>
2022-10-07 19:46:53 +0900

    フォローページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,18 @@ class UsersController < ApplicationController
   def show
     @posts = current_user.posts
   end
+
+  def following
+    @title = "フォロー"
+    @user  = User.find(params[:id])
+    @users = @user.following
+    render "show_follow"
+  end
+
+  def followers
+    @title = "フォロワー"
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render "show_follow"
+  end
 end
