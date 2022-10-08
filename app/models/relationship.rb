756e4fabfd192f3a87df145b27980c6c8e072ba0
Jun-Araki <jun7772006@gmail.com>
2022-10-08 16:49:11 +0900

    フォロー/アンフォロー実装

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,5 +1,6 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
-  validates :following_id, presence: true
+  validates :follower_id, presence: true# rubocop:disable all
+  validates :followed_id, presence: true# rubocop:disable all
 end
