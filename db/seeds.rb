38bd6ef3b62d22afee33404e2a676ea45f057b87
Jun-Araki <jun7772006@gmail.com>
2022-10-09 12:39:11 +0900

    seedデータ変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,10 @@ user1 = User.create!(email: "test1@example.com", password: "password", nickname:
 user2 = User.create!(email: "test2@example.com", password: "password", nickname: "test2")
 user3 = User.create!(email: "test3@example.com", password: "password", nickname: "test3")
 
-Post.create!(name: "真田幸村", catch_copy: "日本一の兵", birthplace: "信濃", castle: "上田城", famous_battle: "大坂の陣",
-             recommend_point: "徳川家康にあと一歩のところまで迫った。その勇姿に多くの人が尊敬した。", image: File.open("./app/assets/images/Sanada_Yukimura.png"), user_id: 1)
-Post.create!(name: "徳川家康", catch_copy: "東照大権現", birthplace: "三河", castle: "江戸城", famous_battle: "関ヶ原の戦い",
-             recommend_point: "270年以上続いた江戸幕府の基盤を築いた", image: File.open("./app/assets/images/Tokugawa_Ieyasu.png"), user_id: 2)
-Post.create!(name: "豊臣秀吉", catch_copy: "太閤", birthplace: "尾張", castle: "大坂城", famous_battle: "山崎の戦い",
-             recommend_point: "農民出身でありながら、関白まで出世して天下統一を果たした", image: File.open("./app/assets/images/Toyotomi_Hideyoshi.png"), user_id: 3)
-Post.create!(name: "織田信長", catch_copy: "第六天魔王", birthplace: "尾張", castle: "安土城", famous_battle: "長篠の戦い",
-             recommend_point: "前例のない方法で天下統一目前まで行った", image: File.open("./app/assets/images/Oda_Nobunaga.png"), user_id: 3)
+post1 = Post.create!(name: "真田幸村", birthplace: "長野県", url: "https://ja.wikipedia.org/wiki/%E7%9C%9F%E7%94%B0%E4%BF%A1%E7%B9%81", image: File.open("./app/assets/images/Sanada_Yukimura.png"), user_id: 1)
+post2 = Post.create!(name: "徳川家康", birthplace: "愛知県", url: "https://ja.wikipedia.org/wiki/%E5%BE%B3%E5%B7%9D%E5%AE%B6%E5%BA%B7", image: File.open("./app/assets/images/Tokugawa_Ieyasu.png"), user_id: 2)
+post3 = Post.create!(name: "豊臣秀吉", birthplace: "愛知県", url: "https://ja.wikipedia.org/wiki/%E8%B1%8A%E8%87%A3%E7%A7%80%E5%90%89", image: File.open("./app/assets/images/Toyotomi_Hideyoshi.png"), user_id: 3)
+post4 = Post.create!(name: "織田信長", birthplace: "愛知県", url: "https://ja.wikipedia.org/wiki/%E7%B9%94%E7%94%B0%E4%BF%A1%E9%95%B7" ,image: File.open("./app/assets/images/Oda_Nobunaga.png"), user_id: 3)
 
 Like.create!(user_id: 1, post_id: 1)
 Like.create!(user_id: 1, post_id: 2)
@@ -18,7 +14,10 @@ Like.create!(user_id: 1, post_id: 3)
 Like.create!(user_id: 2, post_id: 1)
 Like.create!(user_id: 3, post_id: 1)
 
-Comment.create!(content: "前例のない方法で天下統一目前まで行った", user_id:2, post_id:4)
+Comment.create!(post_id:1, user_id:2, content: "徳川家康の首まであと一歩。とても惜しかった。")
+Comment.create!(post_id:2, user_id:2, content: "長い江戸幕府の基礎を作った人")
+Comment.create!(post_id:3, user_id:2, content: "農民出身からのサクセスストーリー")
+Comment.create!(post_id:4, user_id:2, content: "前例をことごとく覆す")
 
 users = User.all
 user  = users.first
