6cb1142221b3c39827302de8cd5eaf6785771386
Jun-Araki <jun7772006@gmail.com>
2022-10-09 14:25:32 +0900

    コメントの削除機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,24 @@
 class CommentsController < ApplicationController
   def create
     @comment = current_user.comments.new(comment_params)
-    redirect_back(fallback_location: root_path)
+    # rubocop:disable all
+    if @comment.save
+      redirect_to request.referer
+    else
+      redirect_to request.referer
+    end
+    # rubocop:enable all
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to request.referer
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id)
+    params.require(:comment).permit(:content, :post_id, :user_id)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
-  has_many :comments# rubocop:disable all
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :birthplace, presence: true, length: { maximum: 30 }
