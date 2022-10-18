c011e06cc15c9d76399e3bd790accb34c25bcf83
Jun-Araki <jun7772006@gmail.com>
2022-10-18 20:35:45 +0900

    いいね一覧作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,31 @@
 class UsersController < ApplicationController
   before_action :logged_in_user
+  before_action :set_user
 
   def show
-    @user = User.find(params[:id])
     @posts = @user.posts
   end
 
   def following
     @title = "フォロー"
-    @user  = User.find(params[:id])
     @users = @user.following
     render "show_follow"
   end
 
   def followers
     @title = "フォロワー"
-    @user  = User.find(params[:id])
     @users = @user.followers
     render "show_follow"
+  end
+
+  def likes
+    likes = Like.where(user_id: @user.id).pluck(:post_id)
+    @like_posts = Post.find(likes)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
