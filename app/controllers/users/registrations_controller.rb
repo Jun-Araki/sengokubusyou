6f26bb45fcc11675b421d5e18499827725dbaf0f
Jun-Araki <jun7772006@gmail.com>
2022-11-04 07:17:35 +0900

    ユーザー編集画面の変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,10 +2,6 @@ module Users
   class RegistrationsController < Devise::RegistrationsController
     before_action :ensure_normal_user, only: %i[update destroy]# rubocop:disable all
 
-    # def update
-    #   redirect_to user_path(current_user)
-    # end
-
     def ensure_normal_user
       redirect_to root_path, alert: "ゲストユーザーは更新・削除できません。" if resource.email == "guest@example.com"# rubocop:disable all
     end
@@ -16,6 +12,10 @@ module Users
 
     def after_update_path_for(resource)
       user_path(resource)
+    end
+
+    def update_resource(resource, params)
+      resource.update_without_password(params)
     end
   end
 end
