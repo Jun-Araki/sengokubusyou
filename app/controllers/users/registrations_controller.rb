611545dc5cbb98ebdfd7bfbce4456cbc0a6029e5
Jun-Araki <jun7772006@gmail.com>
2022-11-03 16:34:41 +0900

    ユーザ新規登録・編集後の画面をuser/showに変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,8 +2,20 @@ module Users
   class RegistrationsController < Devise::RegistrationsController
     before_action :ensure_normal_user, only: %i[update destroy]# rubocop:disable all
 
+    # def update
+    #   redirect_to user_path(current_user)
+    # end
+
     def ensure_normal_user
       redirect_to root_path, alert: "ゲストユーザーは更新・削除できません。" if resource.email == "guest@example.com"# rubocop:disable all
+    end
+
+    def after_sign_up_path_for(resource)
+      user_path(resource)
+    end
+
+    def after_update_path_for(resource)
+      user_path(resource)
     end
   end
 end
