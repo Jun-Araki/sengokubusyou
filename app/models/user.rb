870da7630ee5ae940b6a6c0eb3fd0874a8e58277
Jun-Araki <jun7772006@gmail.com>
2022-11-05 09:42:15 +0900

    ユーザー編集画面のデザイン

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, :email, presence: true
+  validates :profile, length: { maximum: 200 }
 
   mount_uploader :user_image, ImageUploader, uniqueness: true
 
