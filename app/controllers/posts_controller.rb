ae0285d6da41bc89173b7420513025f37a9ff9d5
Jun-Araki <jun7772006@gmail.com>
2022-11-06 18:32:51 +0900

    武将検索ぺーじの条件分岐

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -54,8 +54,8 @@ class PostsController < ApplicationController
   end
 
   def ranks
-    @post_like_ranks    = Post.find(Like.group(:post_id).order("count(post_id) desc").limit(3).pluck(:post_id))
-    @post_comment_ranks = Post.find(Comment.group(:post_id).order("count(post_id) desc").limit(3).pluck(:post_id))
+    @likes    = Post.find(Like.group(:post_id).order("count(post_id) desc").limit(3).pluck(:post_id))
+    @comments = Post.find(Comment.group(:post_id).order("count(post_id) desc").limit(3).pluck(:post_id))
   end
 
   private
