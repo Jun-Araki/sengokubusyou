64c36305d649d7f8df4e76dba327e1f1974bc033
Jun-Araki <jun7772006@gmail.com>
2022-11-08 07:21:35 +0900

    migrateファイル修正

--- db/migrate/20221106015257_change_datatype_furigana_initial_of_post.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDatatypeFuriganaInitialOfPost < ActiveRecord::Migration[6.1]
-  def change
-    change_column :posts, :furigana_initial, "integer USING CAST(furigana_initial AS integer)"# rubocop:disable all
-    change_column :posts, :prefecture_name, "integer USING CAST(prefecture_name AS integer)"# rubocop:disable all
-  end
-end
