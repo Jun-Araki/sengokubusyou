a44c643cb0b96780ff5621a0aa051ec6b089e4ea
Jun-Araki <jun7772006@gmail.com>
2022-11-13 11:00:56 +0900

    ユーザーの武将を修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,9 @@
 class UsersController < ApplicationController
   before_action :logged_in_user
   before_action :set_user
+  before_action :twitter
 
   PER_PAGE = 24
-
-  def show
-    return if @user.twitter.blank?
-
-    twitter_url = "https://twitter.com/"
-    @twitter = twitter_url + @user.twitter
-  end
 
   def following
     @title = "フォローしているユーザー"
@@ -26,23 +20,33 @@ class UsersController < ApplicationController
   def regists
     @title = "登録"
     @posts = @user.posts.page(params[:page]).per(PER_PAGE)
+    render "show_post"
   end
 
   def likes
     @title = "いいね"
     likes = Like.where(user_id: @user.id).pluck(:post_id)
     @posts = Kaminari.paginate_array(Post.find(likes)).page(params[:page]).per(PER_PAGE)
+    render "show_post"
   end
 
   def comments
     @title = "コメント"
     comments = Comment.where(user_id: @user.id).pluck(:post_id)
     @posts = Kaminari.paginate_array(Post.find(comments)).page(params[:page]).per(PER_PAGE)
+    render "show_post"
   end
 
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def twitter
+    return if @user.twitter.blank?
+
+    twitter_url = "https://twitter.com/"
+    @twitter = twitter_url + @user.twitter
   end
 end
