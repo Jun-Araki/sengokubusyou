95944096b52d58307ef4d50e0dc48dad7b28a43c
Jun-Araki <jun7772006@gmail.com>
2022-11-23 09:08:59 +0900

    いいねした武将数の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,19 +6,20 @@ class UsersController < ApplicationController
 
   def show
     @user_posts = @user.posts
-    @posts = @user_posts.page(params[:page]).per(PER_PAGE)
+    @posts = @user.posts.includes(:likes).page(params[:page]).per(PER_PAGE)
   end
 
   def likes
     likes = Like.where(user_id: @user.id).pluck(:post_id)
-    @user_posts = Kaminari.paginate_array(Post.find(likes))
-    @posts = @user_posts.page(params[:page]).per(PER_PAGE)
+    @user_posts = Post.find(likes)
+    @posts = Kaminari.paginate_array(Post.find(likes)).page(params[:page]).per(PER_PAGE)
+    # binding.pry
   end
 
   def comments
     comments = Comment.where(user_id: @user.id).pluck(:post_id)
-    @user_posts = Kaminari.paginate_array(Post.find(comments))
-    @posts = @user_posts.page(params[:page]).per(PER_PAGE)
+    @user_posts = Post.find(comments)
+    @posts = Kaminari.paginate_array(Post.find(comments)).per(PER_PAGE)
   end
 
   def following
