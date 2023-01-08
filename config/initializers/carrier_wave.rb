b1448bae7c6d422156e82095cbe5d7f051f38022
Jun-Araki <jun7772006@gmail.com>
2023-01-08 12:22:22 +0900

    S3に保存できるような設定

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,8 +4,8 @@ if Rails.env.production?
       provider: "AWS",
       aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
       aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      region: Rails.application.credentials.dig(:aws, :region)
+      region: "ap-northeast-1"
     }
-    config.fog_directory = Rails.application.credentials.dig(:aws, :bucket)
+    config.fog_directory = "sengoku-bucket"
   end
 end
